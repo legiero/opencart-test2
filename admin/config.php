541c646cc3eb20deda5af1555987525bb53083be
Alex Erokhin <legi.ero@gmail.com>
2023-08-31 18:31:06 +0300

    Оригинальные файлы CMS

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://host1862984.hostland.pro/admin/');
+define('HTTP_CATALOG', 'http://host1862984.hostland.pro/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://host1862984.hostland.pro/admin/');
+define('HTTPS_CATALOG', 'http://host1862984.hostland.pro/');
+
+// DIR
+define('DIR_APPLICATION', '/home/host1862984/host1862984.hostland.pro/htdocs/www/admin/');
+define('DIR_SYSTEM', '/home/host1862984/host1862984.hostland.pro/htdocs/www/system/');
+define('DIR_IMAGE', '/home/host1862984/host1862984.hostland.pro/htdocs/www/image/');
+define('DIR_STORAGE', '/home/host1862984/host1862984.hostland.pro/htdocs/storage/');
+define('DIR_CATALOG', '/home/host1862984/host1862984.hostland.pro/htdocs/www/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'pdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'host1862984_um2');
+define('DB_PASSWORD', 'sgPRFlmIFI');
+define('DB_DATABASE', 'host1862984_um2');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
